91803e2d27d285fdb3da68ff88c55f9229a45348
killer-cf <costa.kilder@live.com>
2023-09-04 16:28:30 -0300

    entity Product

---
+++ src/domain/store/application/entities/product.ts
@@ -0,0 +1,37 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+interface ProductProps {
+  name: string
+  quantity: number
+  createdAt: Date
+  updatedAt?: Date | null
+}
+
+export class Product extends Entity<ProductProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get quantity() {
+    return this.props.quantity
+  }
+
+  stockIncrement(quantity: number) {
+    this.props.quantity += quantity
+  }
+
+  static create(props: Optional<ProductProps, 'quantity' | 'createdAt'>, id?: UniqueEntityID) {
+    const product = new Product(
+      {
+        name: props.name,
+        quantity: props.quantity ?? 0,
+        createdAt: props.createdAt ?? new Date()
+      },
+      id,
+    )
+
+    return product
+  }
+}
