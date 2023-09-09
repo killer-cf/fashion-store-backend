8390004879f82ef0b88b18a2e42ca29ba3101f3c
killer-cf <costa.kilder@live.com>
2023-09-09 15:59:57 -0300

    add authorization to edit product use case

--- src/domain/store/application/use-cases/edit-product.ts
+++ src/domain/store/application/use-cases/edit-product.ts
@@ -2,28 +2,41 @@ import { Either, left, right } from '@/core/either'
 import { Product } from '../../enterprise/entities/product'
 import { ProductsRepository } from '../repositories/products-repository'
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
+import { AdminsRepository } from '../repositories/admins-repository'
+import { NotAllowedError } from '@/core/errors/not-allowed-error'
 
 interface EditProductUseCaseRequest {
+  adminId: string
   productId: string
   name: string
   price: number
 }
 
 type EditProductUseCaseResponse = Either<
-  ResourceNotFoundError,
+  ResourceNotFoundError | NotAllowedError,
   {
     product: Product
   }
 >
 
 export class EditProductUseCase {
-  constructor(private productsRepository: ProductsRepository) {}
+  constructor(
+    private productsRepository: ProductsRepository,
+    private adminsRepository: AdminsRepository,
+  ) {}
 
   async execute({
+    adminId,
     productId,
     name,
     price,
   }: EditProductUseCaseRequest): Promise<EditProductUseCaseResponse> {
+    const admin = await this.adminsRepository.findById(adminId)
+
+    if (!admin) {
+      return left(new NotAllowedError())
+    }
+
     const product = await this.productsRepository.findById(productId)
 
     if (!product) {
