a3ad6c9b579586530dfd38c90ff3502fc973ddc7
killer-cf <costa.kilder@live.com>
2023-09-11 14:57:11 -0300

    add prisma and docker

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { CreateProductController } from './controllers/create-product.controller'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [CreateProductController],
 })
 export class HttpModule {}
