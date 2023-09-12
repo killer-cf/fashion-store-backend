ab439ab8489e3d70e16fd63c7cbf705444d079fa
killer-cf <costa.kilder@live.com>
2023-09-12 11:15:03 -0300

    test create account

--- src/infra/http/controllers/create-product.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-product.controller.e2e-spec.ts
@@ -1,12 +1,14 @@
 import { AppModule } from '@/infra/app.module'
 import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
 
 describe('Create product (e2e)', () => {
   let app: INestApplication
   let prisma: PrismaService
+  let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -15,20 +17,34 @@ describe('Create product (e2e)', () => {
 
     app = moduleRef.createNestApplication()
 
+    jwt = moduleRef.get(JwtService)
     prisma = moduleRef.get(PrismaService)
 
     await app.init()
   })
 
   test('[POST] /products', async () => {
-    const response = await request(app.getHttpServer()).post('/products').send({
-      name: 'Novo produto',
-      price: 30000,
-      sku: 'adsdasd',
-      brand: 'xiaomi',
-      model: '9t',
-      color: 'red',
+    const user = await prisma.user.create({
+      data: {
+        name: 'jopnh doe',
+        email: 'jonh@doe.com',
+        password: 'password',
+      },
     })
+
+    const accessToken = jwt.sign({ sub: user.id })
+
+    const response = await request(app.getHttpServer())
+      .post('/products')
+      .set('Authorization', 'Bearer ' + accessToken)
+      .send({
+        name: 'Novo produto',
+        price: 30000,
+        sku: 'adsdasd',
+        brand: 'xiaomi',
+        model: '9t',
+        color: 'red',
+      })
 
     expect(response.statusCode).toBe(201)
 
