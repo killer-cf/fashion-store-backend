48f576faaf50cb60c62df2423f615512b6aa82d9
killer-cf <costa.kilder@live.com>
2023-10-17 19:53:29 -0300

    tests create coupon

---
+++ test/repositories/in-memory-coupons-repository.ts
@@ -0,0 +1,22 @@
+import { CouponsRepository } from '@/domain/coupon/application/repositories/coupons-repository'
+import { Coupon } from '@/domain/coupon/enterprise/entities/coupon'
+
+export class InMemoryCouponsRepository implements CouponsRepository {
+  public items: Coupon[] = []
+
+  async findByCode(code: string): Promise<Coupon | null> {
+    const coupon = this.items.find(
+      (coupon) => coupon.code.toLowerCase() === code.toLowerCase(),
+    )
+
+    if (!coupon) {
+      return null
+    }
+
+    return coupon
+  }
+
+  async create(coupon: Coupon): Promise<void> {
+    this.items.push(coupon)
+  }
+}
