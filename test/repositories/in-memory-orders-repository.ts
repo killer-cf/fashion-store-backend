6f27508f1a73bc4498f728a43fc765daa98ee8f9
killer-cf <costa.kilder@live.com>
2023-10-18 12:52:05 -0300

    refactor order to add coupon

--- test/repositories/in-memory-orders-repository.ts
+++ test/repositories/in-memory-orders-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { OrdersRepository } from '@/domain/store/application/repositories/orders-repository'
 import { Order } from '@/domain/store/enterprise/entities/order'
 
@@ -16,5 +17,7 @@ export class InMemoryOrdersRepository implements OrdersRepository {
 
   async create(order: Order): Promise<void> {
     this.items.push(order)
+
+    DomainEvents.dispatchEventsForAggregate(order.id)
   }
 }
