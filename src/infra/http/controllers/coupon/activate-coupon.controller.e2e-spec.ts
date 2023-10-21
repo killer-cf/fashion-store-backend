6efbe1ba83eb5303b99a16e6023ab12f2c09fc5f
killer-cf <costa.kilder@live.com>
2023-10-21 20:32:20 -0300

    controller activate coupon

---
+++ src/infra/http/controllers/coupon/activate-coupon.controller.e2e-spec.ts
@@ -0,0 +1,77 @@
+import { CouponStatus } from '@/domain/coupon/enterprise/entities/value-objects/coupon-status'
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { AdminFactory } from 'test/factories/make-admin'
+import { BrandFactory } from 'test/factories/make-brand'
+import { CouponFactory } from 'test/factories/make-coupon'
+
+describe('Activate coupon (e2e)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let jwt: JwtService
+  let adminFactory: AdminFactory
+  let couponFactory: CouponFactory
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [AdminFactory, CouponFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    jwt = moduleRef.get(JwtService)
+    prisma = moduleRef.get(PrismaService)
+    adminFactory = moduleRef.get(AdminFactory)
+    couponFactory = moduleRef.get(CouponFactory)
+
+    await app.init()
+  })
+
+  test('[PATCH] /coupons/:couponId/activate', async () => {
+    const admin = await adminFactory.makePrismaAdmin()
+    const coupon = await couponFactory.makePrismaCoupon({
+      status: CouponStatus.create('DISABLED'),
+    })
+
+    const accessToken = jwt.sign({ sub: admin.id.toString(), role: 'ADMIN' })
+
+    const response = await request(app.getHttpServer())
+      .patch(`/coupons/${coupon.code}/activate`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .send()
+
+    expect(response.statusCode).toBe(204)
+
+    const couponOnDatabase = await prisma.coupon.findUnique({
+      where: {
+        code: coupon.code,
+      },
+    })
+
+    expect(couponOnDatabase).toBeTruthy()
+    expect(couponOnDatabase?.status).toEqual('ACTIVE')
+  })
+
+  test('[PATCH] /coupons/:couponId/activate (Unauthorized)', async () => {
+    const admin = await adminFactory.makePrismaAdmin({})
+    const coupon = await couponFactory.makePrismaCoupon({
+      code: 'PRIMEIRA',
+      status: CouponStatus.create('DISABLED'),
+    })
+
+    const accessToken = jwt.sign({ sub: admin.id.toString(), role: 'CLIENT' })
+
+    const response = await request(app.getHttpServer())
+      .patch(`/coupons/${coupon.code}/activate`)
+      .set('Authorization', 'Bearer ' + accessToken)
+      .send()
+
+    expect(response.statusCode).toBe(403)
+  })
+})
--- src/infra/http/controllers/coupon/coupon-controllers.module.ts
+++ src/infra/http/controllers/coupon/coupon-controllers.module.ts
@@ -2,10 +2,12 @@ import { CreateCouponUseCase } from '@/domain/coupon/application/use-cases/creat
 import { DatabaseModule } from '@/infra/database/database.module'
 import { Module } from '@nestjs/common'
 import { CreateCouponController } from './create-coupon.controller'
+import { ActivateCouponController } from './activate-coupon.controller'
+import { ActivateCouponUseCase } from '@/domain/coupon/application/use-cases/activate-coupon'
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateCouponController],
-  providers: [CreateCouponUseCase],
+  controllers: [CreateCouponController, ActivateCouponController],
+  providers: [CreateCouponUseCase, ActivateCouponUseCase],
 })
 export class CouponControllersModule {}
