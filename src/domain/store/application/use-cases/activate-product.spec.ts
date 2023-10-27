fdba10f788ef17822833036be51479d43cc758fa
killer-cf <costa.kilder@live.com>
2023-10-27 15:50:08 -0300

    update tests dep

--- src/domain/store/application/use-cases/activate-product.spec.ts
+++ src/domain/store/application/use-cases/activate-product.spec.ts
@@ -3,16 +3,33 @@ import { InMemoryProductsRepository } from 'test/repositories/in-memory-products
 import { makeProduct } from 'test/factories/make-product'
 import { InMemoryProductImagesRepository } from 'test/repositories/in-memory-product-images-repository'
 import { ProductStatus } from '../../enterprise/entities/value-objects/product-status'
+import { InMemoryProductCategoriesRepository } from 'test/repositories/in-memory-product-categories-repository'
+import { InMemoryCategoriesRepository } from 'test/repositories/in-memory-categories-repository'
+import { InMemoryBrandsRepository } from 'test/repositories/in-memory-brands-repository'
+import { InMemoryImagesRepository } from 'test/repositories/in-memory-images-repository'
 
 describe('Activate product', () => {
+  let inMemoryProductCategoriesRepository: InMemoryProductCategoriesRepository
+  let inMemoryCategoriesRepository: InMemoryCategoriesRepository
   let inMemoryProductImagesRepository: InMemoryProductImagesRepository
   let inMemoryProductsRepository: InMemoryProductsRepository
+  let inMemoryBrandsRepository: InMemoryBrandsRepository
+  let inMemoryImagesRepository: InMemoryImagesRepository
   let sut: ActivateProductUseCase
 
   beforeEach(() => {
+    inMemoryProductCategoriesRepository =
+      new InMemoryProductCategoriesRepository()
+    inMemoryCategoriesRepository = new InMemoryCategoriesRepository()
     inMemoryProductImagesRepository = new InMemoryProductImagesRepository()
+    inMemoryBrandsRepository = new InMemoryBrandsRepository()
+    inMemoryImagesRepository = new InMemoryImagesRepository()
     inMemoryProductsRepository = new InMemoryProductsRepository(
       inMemoryProductImagesRepository,
+      inMemoryBrandsRepository,
+      inMemoryImagesRepository,
+      inMemoryProductCategoriesRepository,
+      inMemoryCategoriesRepository,
     )
     sut = new ActivateProductUseCase(inMemoryProductsRepository)
   })
