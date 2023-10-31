a0d2cfdba37e52f9dd346eb6b706dccc0a5db7f8
killer-cf <costa.kilder@live.com>
2023-10-31 19:11:16 -0300

    refactor validate coupon controller

--- src/infra/http/controllers/coupon/validate-coupon.controller.ts
+++ src/infra/http/controllers/coupon/validate-coupon.controller.ts
@@ -13,6 +13,9 @@ import { ZodValidationPipe } from '@/infra/pipes/zod-validation-pipe'
 import { CouponSoldOutError } from '@/domain/coupon/application/use-cases/errors/coupon-sold-out-error'
 import { CouponExpiredError } from '@/domain/coupon/application/use-cases/errors/coupon-expired-error'
 import { CouponMinValueError } from '@/domain/coupon/application/use-cases/errors/coupon-min-value-error'
+import { OrdersRepository } from '@/domain/store/application/repositories/orders-repository'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
 
 const valueQueryParamSchema = z
   .string()
@@ -25,17 +28,27 @@ type ValueQueryParamSchema = z.infer<typeof valueQueryParamSchema>
 
 @Controller('/coupons/:code/validate')
 export class ValidateCouponController {
-  constructor(private validateCoupon: ValidateCouponUseCase) {}
+  constructor(
+    private validateCoupon: ValidateCouponUseCase,
+    private ordersRepository: OrdersRepository,
+  ) {}
 
   @Get()
-  @Roles(['ADMIN'])
+  @Roles(['CLIENT'])
   async handle(
     @Param('code') code: string,
     @Query('value', valueQueryValidationPipe) value: ValueQueryParamSchema,
+    @CurrentUser() user: UserPayload,
   ) {
+    const clientOrders = await this.ordersRepository.findManyByClientId(
+      user.sub,
+    )
+
     const result = await this.validateCoupon.execute({
       code,
       value,
+      isFirstOrder: clientOrders.length === 0,
+      alreadyBeenUsed: clientOrders.some((order) => order.couponCode === code),
     })
 
     if (result.isLeft()) {
